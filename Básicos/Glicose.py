a4918572087d5f67618be68fd9139d3aee295e54
giomara20 <gilmaracorreia00@gmail.com>
2022-12-26 22:40:17 -0300

    adicionando exercícicos básicos ao repo

---
+++ Básicos/Glicose.py
@@ -0,0 +1,11 @@
+
+glicose = float(input("Digite a medida da glicose: "))
+
+if glicose <= 100:
+ print("Normal")
+
+elif glicose  > 100 and glicose <= 140:
+ print("Elevada")
+
+else: 
+ print("Diabetes")
---
+++ Básicos/Área de um círculo.py
@@ -0,0 +1,6 @@
+
+raio = int(input("Digite o valor do raio do circulo:  "))
+pi = 3.14
+area = float(pi * (raio * raio))
+
+print("A área do circulo é", area)
